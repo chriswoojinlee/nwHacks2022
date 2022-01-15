169ce1ffc2455632a24cf5aa6e2265dd88afd336
zhanginc <zhanginc@students.cs.ubc.ca>
2022-01-15 14:57:13 -0800

    adds button, and pages

---
+++ thephishnet/src/pages/Main.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import Button from '../components/Button';
+import {Container, Wrapper} from '../pages/Page';
+
+function Main() {
+    return(
+        <Container>
+            <Wrapper>
+
+                
+            </Wrapper>
+        </Container>
+    )
+
+}
+
+export default Main;
---
+++ thephishnet/src/pages/Page.js
@@ -0,0 +1,33 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+
+position: relative;
+width: 1512px;
+height: 982px;
+
+background: #A3E6FF;
+`
+
+export const Wrapper = styled.div`
+display: grid;
+z-index: 1;
+height: 860px;
+width: 100%;
+max-width: 1100px;
+margin-right: auto;
+margin-left: auto;
+padding: 0 24px;
+justify-content: center;
+`
+
+export const ImgWrap = styled.div`
+  max-width: 555px;
+  height: 100%;
+  `
+
+  export const Img = styled.img`
+  width: 100%;
+  margin: 0 0 10px 0;
+  padding-right: 0;
+  `
